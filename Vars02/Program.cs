e4143a056813d497121f4fcfbc5ebbf8a85da674
Dacheng Gao <successgdc@gmail.com>
2025-02-03 00:44:28 +0800

    add Vars02
    decimal and its internal struct

---
+++ Vars02/Program.cs
@@ -0,0 +1,31 @@
+﻿// decimal uses 16-byte to store more precisions (28 to 29).
+// The internal fields of decimal are:
+// * 4-byte for flags (1 bit for sign and 7 bits preserved, 1-byte for scale, and the next 2-byte preserved),
+// * 4-byte for hi,
+// * 8-byte for lo,
+
+namespace Vars02;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal max = Decimal.MaxValue;
+        Console.WriteLine(max);
+
+        decimal a = 0x1234;
+        Console.WriteLine(a);
+        decimal a2 = -0x1234;
+        Console.WriteLine(a2);
+
+        decimal b = 0x1122334455667700;
+        Console.WriteLine(b);
+
+        decimal c = new decimal(0x11112222, 0x33334444, 0x55556666, false, 8);
+        Console.WriteLine(c);
+
+        // math
+        decimal sum = a + a2;
+        Console.WriteLine(sum);
+    }
+}
