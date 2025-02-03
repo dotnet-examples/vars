62bd19105307337a12cd62d08493eb1b8a3e04b5
Dacheng Gao <successgdc@gmail.com>
2025-02-03 18:23:43 +0800

    add Vars03
    introduce type: char

---
+++ Vars03/Program.cs
@@ -0,0 +1,18 @@
+﻿// char is used to store single character,
+// it is 2-byte long, from \u0000 (\0) to \uffff.
+
+namespace Vars03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char ch = 'g';
+        char ch2 = '高';
+        char ch3 = '\u1234';
+
+        Console.WriteLine(ch);
+        Console.WriteLine(ch2);
+        Console.WriteLine(ch3);
+    }
+}
